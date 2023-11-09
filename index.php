2d0d0354b77f5e1c27e283eabba4eeb304363033
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-11-09 18:45:41 +0100

    Add user table and modal for creating new users

--- index.php
+++ index.php
@@ -34,12 +34,76 @@
     </div>
   </div>
 </div>
+<br/>
+<br/>
+<div class="table-responsive">
+    <table id="datos_usuario" class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Email</th>
+                <th>Imagen</th>
+                <th>Fecha de Creación</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
 
   <footer class="text-muted py-4">
     <div class="container">
       <p class="mb-0">Pie de página</p>
     </div>
   </footer>
+
+  <!-- Modal -->
+<!-- Modal -->
+<div class="modal fade" id="modalUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Crear Usuario</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form  method="POST" id="formulario" enctype="multipart/form-data">
+          <div class="modal-content">
+            <div class="modal-body">
+              <label for="nombre">Ingrese el nombre</label>
+              <input type="text" name="nombre" id="nombre" class="form-control">
+              <br />
+              <label for="apellidos">Ingrese los apellidos</label>
+              <input type="text" name="apellidos" id="apellidos" class="form-control">
+              <br />
+
+              <label for="telefono">Ingrese el teléfono</label>
+              <input type="text" name="telefono" id="telefono" class="form-control">
+              <br />
+
+              <label for="email">Ingrese el email</label>
+              <input type="email" name="telefono" id="email" class="form-control">
+              <br />
+              <label for="imagen">Seleccione una imagen</label>
+              <input type="file" name="imagen_usuario" id="imagen_usuario" class="form-control">
+              <span id="imagen-subida"></span>
+              <br />
+            </div>
+            <div class="modal-footer">
+              <input type="hidden" name="id_usuario" id="id_usuario">
+              <input type="hidden" name="operacion" id="operacion">
+              <input type="submit" name="action" id="action" class="btn btn-success" value="Crear">
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
   <!-- JavaScript -->
     <script
